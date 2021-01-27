5f754cdafe22d5ea792c100b59371f8ac353b8d7
MartinNevlaha <martin.nevlaha@gmail.com>
2021-01-27 21:16:19 +0100

    finish single container

--- src/containers/SignleDice.jsx
+++ src/containers/SignleDice.jsx
@@ -1,17 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 import DiceElement from "../components/Dice/DiceElement";
 import Button from "../components/UI/Button/Button";
+import {generateNumber} from "../utils/utility";
 
-const Dice = props => {
-  const [number, setNumber] = useState(null)
+const Dice = (props) => {
+  const [number, setNumber] = useState(null);
+
+  
+  const clickHandler = () => {
+    setNumber(generateNumber());
+  };
 
   return (
     <React.Fragment>
-      <DiceElement thrownNumber={1}/>
-      <Button iconType="roll" >Roll dice</Button>
+      <DiceElement thrownNumber={number} />
+      <Button iconType="roll" clicked={clickHandler}>
+        Roll dice
+      </Button>
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default Dice;
