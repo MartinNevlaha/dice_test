ea5cce6e1046cd6062be3d529724275166d1f951
MartinNevlaha <martin.nevlaha@gmail.com>
2021-01-27 18:54:53 +0100

    styling logo in navbar

--- src/HOC/Layout.jsx
+++ src/HOC/Layout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import classes from "./Layout.module.scss";
+import Navbar from "../components/Navbar/Navbar";
 
 const Layout = (props) => {
   return (
     <div className={classes.Layout}>
+      <Navbar />
       <main>{props.chidren}</main>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,24 @@
-import classes from "*.module.css";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDiceD6 } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
 
+import classes from "./Navbar.module.scss";
 
-const UserChoice = props => {
-  
+const UserChoice = (props) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className={classes.navbar}>
+      <div className={classes.navbar_logo}>
+        <FontAwesomeIcon icon={faDiceD6}  size="2x"/>
+        <h2>Dice</h2>
+      </div>
+      <ul className={classes.navbar_links}>
+        <li>Single dice</li>
+        <li>Custom dice</li>
+        <li>Multiple dice</li>
+      </ul>
+    </nav>
+  );
+};
 
 export default UserChoice;
